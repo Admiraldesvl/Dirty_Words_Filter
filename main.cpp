4ed91aef9541a64dde3fed5856b476d02032f8d0
Admiraldesvl <40477748+Admiraldesvl@users.noreply.github.com>
2018-08-07 19:39:42 +0800

    Firstly uploaded.

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "src/Filter.h"
+
+using namespace std;
+
+int main() {
+    cout << "Press Q to stop..." << endl;
+    string line;
+    map<string,int> words;
+    while (getline(cin, line)) {
+        if (line == "Q") {
+            break;
+        }
+        words = split(line);
+        if (dirtyList(words)) { // Once dirty words inside, do the filter; if not, print the original line.
+            filter(words, line);
+        }
+        cout << line << endl;
+    }
+    return 0;
+}
