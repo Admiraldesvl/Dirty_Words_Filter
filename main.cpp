f17ba10800e602aa7be8fcabca8f38d565639066
Admiraldesvl <40477748+Admiraldesvl@users.noreply.github.com>
2018-08-11 15:43:39 +0800

    Add function to block too many uppercase words.
    If all the words are uppercase, they will be blocked as dirty words.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    cout << "Enter words to filter. Press Q to stop..." << endl;
+    cout << "Press Q to stop..." << endl;
     string line;
     map<string,int> words;
     while (getline(cin, line)) {
@@ -12,7 +12,7 @@ int main() {
             break;
         }
         words = split(line);
-        if (dirtyList(words)) { // Once dirty words inside, do the filter; if not, print the original line.
+        if (isPureUpper(words) || dirtyList(words)) { // Once dirty words inside, or all the words are uppercase, do the filter; if not, print the original line.
             filter(words, line);
         }
         cout << line << endl;
